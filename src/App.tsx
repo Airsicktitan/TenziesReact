1bf8edb258dbe913db7fb367b6690cec221d72ba
airsicktitan <adamcwhooper@yahoo.com>
2024-10-31 17:43:10 -0400

    Tenzies Game in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {useState} from "react";
 import ReactConfetti from "react-confetti";
+import {Simulate} from "react-dom/test-utils";
+import play = Simulate.play;
+import {map} from "react-confetti/dist/types/utils";
 
 function App() {
     const randomNum:(min: number, max: number) => number = (min: number, max: number): number => {
@@ -12,6 +15,8 @@ function App() {
     const[frozen, setFrozen] = useState(Array(10).fill(false));
     const[count, setCount] = useState(0);
     const[isWin, setIsWin] = useState(false);
+    const[playerNumber, setPlayerNumber] = useState<number>(0);
+    const[isSelected, setIsSelected] = useState<boolean>(true);
 
     const handleOnClick: ()=> void = (): void => {
         if(isWin) return;
@@ -20,36 +25,45 @@ function App() {
         setNum(newNumbs);
         setCount(count + 1);
 
-        if(newNumbs.every((x) => x === 1)) setIsWin(true);
+        if(newNumbs.every((x) => x === playerNumber)) setIsWin(true);
     }
 
     const toggleFreeze:(index: number) => void = (index: number): void => {
         if(isWin) return;
 
-
         setFrozen(frozen.map((freeze: number, i: number): any => (i === index ? !freeze : freeze)));
     }
+    const handleSquareClick = (index: number): void => {
+        if(isSelected) {
+            setPlayerNumber(num[index])
+            toggleFreeze(index)
 
+            setIsSelected(false);
+            console.log(playerNumber);
+        }
+        else {
+            toggleFreeze(index);
+        }
+    }
   return (
     <>
         {isWin && <ReactConfetti />}
         <h1>Tenzies Game!</h1>
+        <p>Select a number and get all ten squares to match!</p>
         <div className="board-row">
-            <Square value = { num[0] } frozen = { frozen[0] } onClick={ (): void => toggleFreeze(0)  } isWin= {isWin} />
-            <Square value = { num[1] } frozen = { frozen[1] } onClick={ (): void => toggleFreeze(1)  } isWin= {isWin}  />
-            <Square value = { num[2] } frozen = { frozen[2] } onClick={ (): void => toggleFreeze(2)  } isWin= {isWin}  />
-            <Square value = { num[3] } frozen = { frozen[3] } onClick={ (): void => toggleFreeze(3)  } isWin= {isWin}  />
-            <Square value = { num[4] } frozen = { frozen[4] } onClick={ (): void => toggleFreeze(4)  } isWin= {isWin}  />
-        </div>
-        <div className="board-row">
-            <Square value = { num[5] } frozen = { frozen[5] } onClick={ (): void => toggleFreeze(5)  } isWin= {isWin}  />
-            <Square value = { num[6] } frozen = { frozen[6] } onClick={ (): void => toggleFreeze(6)  } isWin= {isWin}  />
-            <Square value = { num[7] } frozen = { frozen[7] } onClick={ (): void => toggleFreeze(7)  } isWin= {isWin}  />
-            <Square value = { num[8] } frozen = { frozen[8] } onClick={ (): void => toggleFreeze(8)  } isWin= {isWin}  />
-            <Square value = { num[9] } frozen = { frozen[9] } onClick={ (): void => toggleFreeze(9)  } isWin= {isWin}  />
+            {num.map((n, index) => (
+                <Square
+                    key={index}
+                    value = {n}
+                    frozen = {frozen[index]}
+                    onClick={(): void => handleSquareClick(index)}
+                    isWin= {isWin}
+                    isSelected= {isSelected}
+                    playerNumber= {playerNumber} />
+                ))}
         </div>
         <div className="board-row-Gen">
-            {!isWin && (<button className="button-gen" onClick={handleOnClick} disabled={isWin}>Click to generate new
+            {!isWin && (<button className="button-gen" onClick={handleOnClick} disabled={playerNumber === null}>Click to generate new
                 number!</button>)}
             {isWin && (<button className="button-gen-reset" onClick={(): void => window.location.reload()}>Reset</button>)}
             <p className="p-gen">Rolls: {count}</p>
@@ -58,8 +72,8 @@ function App() {
   )
 }
 
-function Square( { value, frozen, onClick, isWin }: {value: number, frozen: boolean, onClick: any, isWin: boolean} ) {
-    return <button className={`button ${frozen ? 'frozen' : ''} ${isWin && value === 1 ? 'win' : ''}`} onClick={onClick} disabled={isWin}>{ value }</button>
+function Square( { value, frozen, onClick, isWin, isSelected, playerNumber }: {value: number, frozen: boolean, onClick: any, isWin: boolean, isSelected: boolean, playerNumber: number} ) {
+    return <button className={`button ${frozen ? 'frozen' : ''} ${isWin && value === playerNumber ? 'win' : ''} ${isSelected ? 'selecting' : ''}`} onClick={onClick} disabled={isWin}>{ value }</button>
 }
 
 export default App
